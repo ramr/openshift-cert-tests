dc32eb45a09a8eca7d308e18044181242f53329f
ramr <smitram@gmail.com>
2016-11-02 17:09:21 -0700

    Add scripts for generating certs.

--- settings.sh
+++ settings.sh
@@ -12,11 +12,14 @@ CASUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=root.ca.org"
 # Intermediate CA subject.
 INTCASUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=intermediate.ca.org"
 
-# Test Certificate subject.
-SUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=www.ca.test"
+# Test Certificate CN and subject.
+CN="www.ca.test"
+SUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=${CN}"
 
-# Test Certificate subject.
-EXPIREDSUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=expired.ca.test"
+# Test Certificate CN and subject.
+EXPIREDCN="expired.ca.test"
+EXPIREDSUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=${EXPIREDCN}"
 
-# Future validity Certificate subject.
-FUTUREVALIDSUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=future.ca.test"
+# Future validity Certificate CN and subject.
+FUTUREVALIDCN="valid.in.the.future.ca.test"
+FUTUREVALIDSUBJECT="/C=US/ST=CA/O=Security/OU=OpenShift3 test CA/CN=${FUTUREVALIDCN}"
